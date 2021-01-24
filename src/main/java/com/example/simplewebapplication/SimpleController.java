8ccc96d7dab3e1ede73d7a59bc7b03bc02d96998
ProgrammingPortfolio1 <70398262+ProgrammingPortfolio1@users.noreply.github.com>
2021-01-24 02:31:05 +0200

    Initial commit

---
+++ src/main/java/com/example/simplewebapplication/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.simplewebapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping("/")
+    public  String index(){
+
+        return "Hello Intellij Java";
+    }
+
+
+}
